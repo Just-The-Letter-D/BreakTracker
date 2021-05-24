322b12f5aac524609f59301215cc4a145b93548f
KuhnTran <KuhnTran@gmail.com>
2021-05-23 23:09:01 -0700

    Updates

---
+++ precache-manifest.4eb822d9bf26008902994b9a379162e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fe68705754471df2f508f60ee2ca2ee",
+    "url": "/LegacyBreaktimeManagement/index.html"
+  },
+  {
+    "revision": "45dc4ae6d1f8e967751c",
+    "url": "/LegacyBreaktimeManagement/static/css/main.edc3b5e3.chunk.css"
+  },
+  {
+    "revision": "4bb9e6ae74c730bb6c05",
+    "url": "/LegacyBreaktimeManagement/static/js/2.88f45ee1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/LegacyBreaktimeManagement/static/js/2.88f45ee1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45dc4ae6d1f8e967751c",
+    "url": "/LegacyBreaktimeManagement/static/js/main.54f1b65c.chunk.js"
+  },
+  {
+    "revision": "6151622da87fd25ec6bb",
+    "url": "/LegacyBreaktimeManagement/static/js/runtime-main.c0628bb6.js"
+  }
+]);
